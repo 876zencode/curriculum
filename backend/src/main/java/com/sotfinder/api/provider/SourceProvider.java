329b5360312e25fdd13588a4faf2dcfe189e7099
Che-Andre Gordon <876zencode@gmail.com>
2025-11-19 07:49:24 -0500

    chore: Code cleanup

--- backend/src/main/java/com/sotfinder/api/provider/SourceProvider.java
+++
@@ -1,8 +0,0 @@
-package com.sotfinder.api.provider;
-
-import com.sotfinder.api.model.RawSourceResult;
-import java.util.List;
-
-public interface SourceProvider {
-    List<RawSourceResult> fetchRawResults(String query);
-}
--- backend/src/main/java/com/sotfinder/api/service/SourceSearchService.java
+++
@@ -1,8 +0,0 @@
-package com.sotfinder.api.service;
-
-import com.sotfinder.api.dto.SourceDTO;
-import java.util.List;
-
-public interface SourceSearchService {
-    List<SourceDTO> searchSources(String query);
-}
